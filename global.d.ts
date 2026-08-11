0b36ec946a679b9750647a18bcfa9340640462ac
engrsakib <snazmussakib01@gmail.com>
2026-08-11 18:54:48 +0600

    build error resolve

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,23 @@ declare namespace JSX {
   }
 }
 
+declare namespace React {
+  namespace JSX {
+    interface IntrinsicElements {
+      "math-field": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          "read-only"?: string | boolean;
+        },
+        HTMLElement
+      >;
+    }
+  }
+}
+
+interface Window {
+  MathfieldElement?: typeof import("mathlive").MathfieldElement;
+}
+
 declare module "*.css";
 declare module "*.scss";
 declare module "*.sass";
